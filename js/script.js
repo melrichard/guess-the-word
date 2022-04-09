ae6004c3e7206a713935f7b3d15eac6ae7b5bfce
richard_m <mel.richard.4@gmail.com>
2022-04-08 17:10:13 -0700

    Added validate and capture input functions

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ const message = document.querySelector(".message"); // message paragraph
 const playAgainButton = document.querySelector(".play-again"); //hiden button that appear to play again
 
 const word = "magnolia";
+const guessedLetters = [];
 
 //function for placeholders
 
@@ -31,4 +32,41 @@ guessButton.addEventListener("click", function(e){
     const guess = letterInput.value;
     console.log(guess);
     letterInput.value = "";
+    //empty message
+    message.innerText = "";
+
+    const goodGuess = playerInput (guess);
+    
+    if (goodGuess) {
+        makeGuess(guess);
+    };
+    
 });
+
+//validate the input is a letter
+const playerInput = function(input){
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0){
+        //input is empty
+        message.innerText = "Please enter a letter.";
+    }
+    else if (input.length > 1){
+        //more than one letter
+        message.innerText = "Please enter only 1 letter at a time!";
+    }
+    else if (!input.match(acceptedLetter)){
+       message.innerText = "Please enter a letter from a to z!";
+    } else {return input};
+};
+
+//capture the input
+
+const makeGuess = function (letter){
+    letter.toUpperCase();
+    if (guessedLetters.includes(letter)){
+        message.innerText = "You already guessed this letter!";
+    } else {
+        guessedLetters.push(letter);
+        console.log(guessedLetters);
+    };
+};
