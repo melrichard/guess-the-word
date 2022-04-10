fc7ea094af2d79c70171701ade55debdd11ce713
richard_m <mel.richard.4@gmail.com>
2022-04-10 10:36:08 -0700

    Added remaining guesses function and async function

--- js/script.js
+++ js/script.js
@@ -4,13 +4,30 @@ const guessedLettersList = document.querySelector(".guessed-letters"); // ul
 const guessButton = document.querySelector(".guess"); // guess button
 const letterInput = document.querySelector(".letter"); // input by player
 const wordInProgress = document.querySelector(".word-in-progress"); // paragraph
-const remainingGuesses = document.querySelector(".remaining"); // paragraph with remaining guesses
+const remainingGuessesElement = document.querySelector(".remaining"); // paragraph with remaining guesses
 const remainingGuessesSpan = document.querySelector(".remaining span"); // span with #
 const message = document.querySelector(".message"); // message paragraph
 const playAgainButton = document.querySelector(".play-again"); //hiden button that appear to play again
 
-const word = "magnolia";
+let word = "magnolia";
 const guessedLetters = [];
+let remainingGuesses = 8;
+
+//Fetch words from file
+const getWord = async function(){
+    const response = await fetch (
+        "https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+    const words = await response.text();
+    //console.log(words);
+    const wordArray = words.split("\n");
+    //console.log(wordArray);
+    const randomIndex = Math.floor(Math.random() * wordArray.length);
+    word = wordArray[randomIndex].trim();
+    placeHolder(word);
+};
+
+getWord()
+
 
 //function for placeholders
 
@@ -22,8 +39,6 @@ const placeHolder = function (word){
         };
         wordInProgress.innerText = placeHolderLetters.join("");
 };
-
-placeHolder(word);
 
 //Event Listener
 
@@ -69,6 +84,7 @@ const makeGuess = function (letter){
         guessedLetters.push(letter);
         console.log(guessedLetters);
         showGuessedLetters();
+        remainingGuessesCount(letter);
         updateWordInProgress(guessedLetters);
     }
 };
@@ -104,5 +120,28 @@ const checkWin = function(){
     if (word.toUpperCase () === wordInProgress.innerText){
         message.classList.add("win");
         message.innerHTML = '<p class="highlight">You guessed correct the word! Congrats!</p>';
+    };
+};
+
+//function to count remainig guesses
+const remainingGuessesCount = function(guess){
+    const wordUpper = word.toUpperCase();
+    if (!wordUpper.includes(guess)){
+        message.innerText = `Sorry, the word has no letter ${guess}`;
+        remainingGuesses -= 1;
+        ;
+    } else {
+        message.innerText = `Good Guess! The word has the letter ${guess}.`;
+    };
+
+    if (remainingGuesses === 0) {
+        message.innerText = `Game Over! The word was ${wordUpper}`;
+        remainingGuessesElement.innerText = "";
+    } 
+    else if (remainingGuesses === 1){
+        remainingGuessesSpan.innerText = `only ${remainingGuesses} guess`;
+    } 
+    else {
+        remainingGuessesSpan.innerText = `${remainingGuesses} guesses`;
     };
 };
